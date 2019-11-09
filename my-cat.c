e12d0a18e16cace7528b788e079635a88b247e9b
mikkomajamaa <mikko.majamaa@outlook.com>
2019-11-09 09:43:51 +0200

    commit working version of my-cat

--- my-cat.c
+++ my-cat.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  int i, c;
+
+  for (i = 1; i < argc; i++) {
+    fp = fopen(argv[i], "r");
+
+    // failed to open file
+    if (fp == NULL) {
+      printf("my-cat: cannot open file\n");
+      exit(1);
+    }
+
+    // read and print file's content
+    while ((c = getc(fp)) != EOF) {
+      putchar(c);
+    }
+
+    fclose(fp);
+  }
 
   return 0;
 }
